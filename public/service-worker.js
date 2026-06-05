af767137c14b31404dd52aa6318d07c081433719
GON Uniformes <gerencia@worklife.com.mx>
2026-06-05 16:52:27 -0600

    fix: service worker en carpeta public

---
+++ public/service-worker.js
@@ -0,0 +1,6 @@
+/* eslint-disable no-restricted-globals */
+import { clientsClaim } from 'workbox-core';
+import { precacheAndRoute } from 'workbox-precaching';
+
+clientsClaim();
+precacheAndRoute(self.__WB_MANIFEST);

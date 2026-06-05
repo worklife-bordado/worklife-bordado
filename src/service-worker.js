5c3e536cb81adea08e1bdcb8affc58e26e581ecf
GON Uniformes <gerencia@worklife.com.mx>
2026-06-05 15:29:07 -0600

    feat: PWA service worker y manifest

---
+++ src/service-worker.js
@@ -0,0 +1,17 @@
+const CACHE_NAME = "worklife-bordado-v1";
+
+self.addEventListener("install", event => {
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))
+    )
+  );
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(fetch(event.request).catch(() => caches.match(event.request)));
+});

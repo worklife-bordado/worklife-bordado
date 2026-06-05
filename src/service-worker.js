b8b5fd1d34bf1c54928b28d9940f022bb01bc2d7
GON Uniformes <gerencia@worklife.com.mx>
2026-06-05 16:38:15 -0600

    fix: service worker compatible con CRA

--- src/service-worker.js
+++ src/service-worker.js
@@ -1,17 +1,6 @@
-const CACHE_NAME = "worklife-bordado-v1";
+/* eslint-disable no-restricted-globals */
+import { clientsClaim } from 'workbox-core';
+import { precacheAndRoute } from 'workbox-precaching';
 
-self.addEventListener("install", event => {
-  self.skipWaiting();
-});
-
-self.addEventListener("activate", event => {
-  event.waitUntil(
-    caches.keys().then(keys =>
-      Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))
-    )
-  );
-});
-
-self.addEventListener("fetch", event => {
-  event.respondWith(fetch(event.request).catch(() => caches.match(event.request)));
-});
+clientsClaim();
+precacheAndRoute(self.__WB_MANIFEST);
